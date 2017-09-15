e9b971d8655e94e7653f534a005fcdb4b5d428e0
Zachary Epps <zactepps@gmail.com>
2017-09-14 22:10:43 -0400

    added readline functions and mode changing ability

--- main.py
+++ main.py
@@ -6,12 +6,35 @@ from pathlib import Path
 from sys import stdout
 from os import name
 from configparser import ConfigParser
+import readline
 
 conf = ConfigParser()
 
 CONF_FILE = Path("./config.ini")
 INSTANCE = "https://mastodon.social"
 NAME = "TootREPL"
+
+CMDS = [ '!emacs', '!vi', '!quit', '!q', '!help' ]
+MODE_CMDS = CMDS[:2]
+
+INTRO = """Welcome! Type out a status and hit enter to post it!
+
+To quit just type !quit
+
+If you want to make a thread, at the beginning of your status add '!thread'
+
+You can supply a dictionary in the form of 'KEY=VALUE, KEY2=VALUE2, etc'
+ but start the string with !dict
+
+"""
+
+
+
+# updates readline mode
+def change_readline_mode(newMode):
+    newMode = newMode[1:]
+    if newMode in [ 'emacs', 'vi' ]:
+        readline.parse_and_bind('set editing-mode ' + newMode)
 
 
 # saves the config file again
@@ -81,33 +104,30 @@ client = login()
 user_in = ''
 last_post = None
 
-print("""Welcome! Type out a status and hit enter to post it!
-
-To quit just type !quit
-
-If you want to make a thread, at the beginning of your status add '!thread'
-
-You can supply a dictionary in the form of 'KEY=VALUE, KEY2=VALUE2, etc'
- but start the string with !dict
-
-""")
+print(INTRO)
 while user_in not in ['!quit', '!q']:
     user_in = input("Toot> ")
 
-    if user_in.startswith('!dict'):
-        try:
-            ## converts the string into a dict
-            user_in = dict( (k.strip(), v.strip()) for k,v in (j.split('=') for j in user_in[6:].split(',') ) )
-            last_post = client.status_post(**user_in)
-        except TypeError:
-            print("Whoops, you entered a key that you shouldn't have.\nProper keys are status, visibility, spoiler_text")
-    else:
-        if user_in.startswith('!thread'):
-            last_post = client.status_post(user_in[8:], in_reply_to_id= last_post)
-        elif user_in != '!quit' and user_in != '':
-            last_post = client.status_post(user_in)
+    if user_in.split()[0] not in CMDS:
+        if user_in.startswith('!dict'):
+            try:
+                ## converts the string into a dict
+                user_in = dict( (k.strip(), v.strip()) for k,v in (j.split('=') for j in user_in[6:].split(',') ) )
+                last_post = client.status_post(**user_in)
+            except TypeError:
+                print("Whoops, you entered a key that you shouldn't have.\nProper keys are status, visibility, spoiler_text")
         else:
-            last_post = None
+            if user_in.startswith('!thread'):
+                last_post = client.status_post(user_in[8:], in_reply_to_id= last_post)
+            elif user_in != '':
+                last_post = client.status_post(user_in)
+            else:
+                last_post = None
+    else:
+        if user_in in MODE_CMDS:
+            change_readline_mode(user_in)
+        if user_in == '!help':
+            print(INTRO)
     
     if last_post is not None:
         last_post = last_post['id']
